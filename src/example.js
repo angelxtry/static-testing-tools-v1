1060197d6c03f76e13f3cf1876dc267c78599690
angelxtry <angelxtry@gmail.com>
2020-12-16 21:19:00 +0900

    first commit

---
+++ src/example.js
@@ -0,0 +1,10 @@
+const username = 'freddy';
+typeof username === 'string';
+
+if ('serviceWorker' in navigator) {
+  console.log('ok serviceWorker');
+}
+
+const greeting = 'hello';
+console.log(`${greeting} world`);
+[1, 2, 3].forEach((x) => console.log(x));
